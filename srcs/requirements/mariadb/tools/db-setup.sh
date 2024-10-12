8532cda19c8b1c7d58ebaa971d58f605d001cfae
Liashkov S <s.liashkov@twslabs.org>
2024-10-12 16:14:25 +0700

    made maria run as server; need to debug wp connection to db

--- srcs/requirements/mariadb/tools/db-setup.sh
+++ srcs/requirements/mariadb/tools/db-setup.sh
@@ -4,25 +4,28 @@
 echo "Available services in container:"
 service --status-all
 
-# Start MariaDB service
+# Start MariaDB service in the background
 echo "Starting MariaDB service..."
 service mariadb start
 
 # Wait for MariaDB to fully start
 echo "Waiting for MariaDB to start..."
-until mysqladmin ping --silent; do
+until mysqladmin ping --silent -h localhost; do
   echo "Waiting for MariaDB to start..."
   sleep 1
 done
 
 # Create the admin user using environment variables
 echo "Creating Admin User: $MARIADB_ADMIN_LOGIN"
+
 mysql -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_ADMIN_LOGIN}'@'%' IDENTIFIED BY '${MARIADB_ADMIN_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${MARIADB_ADMIN_LOGIN}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Leave MariaDB running in the foreground
-echo "MariaDB setup complete. Running MariaDB in the foreground."
+service mariadb stop
 
-exec mysqld_safe
+
+# Start MariaDB in safe mode and keep it running in the foreground
+echo "Starting MariaDB in safe mode (foreground)..."
+exec mysqld_safe --skip-grant-tables
