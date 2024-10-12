dc4d8f099e4777347a177f65a8391653c6f55880
Liashkov S <s.liashkov@twslabs.org>
2024-10-12 15:55:17 +0700

    made mariadb exit with 0; need to make it a server

--- srcs/requirements/mariadb/tools/db-setup.sh
+++ srcs/requirements/mariadb/tools/db-setup.sh
@@ -4,28 +4,25 @@
 echo "Available services in container:"
 service --status-all
 
-# Start MariaDB service in the background
+# Start MariaDB service
 echo "Starting MariaDB service..."
 service mariadb start
 
 # Wait for MariaDB to fully start
+echo "Waiting for MariaDB to start..."
 until mysqladmin ping --silent; do
   echo "Waiting for MariaDB to start..."
   sleep 1
 done
 
 # Create the admin user using environment variables
-echo "Creating Admin User:" $MARIADB_ADMIN_LOGIN
-
-mysql -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_ADMIN_LOGIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASSWORD}';"
+echo "Creating Admin User: $MARIADB_ADMIN_LOGIN"
+mysql -u root -e "CREATE USER IF NOT EXISTS '${MARIADB_ADMIN_LOGIN}'@'%' IDENTIFIED BY '${MARIADB_ADMIN_PASSWORD}';"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_ADMIN_LOGIN}'@'localhost' IDENTIFIED BY '${MARIADB_ADMIN_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '${MARIADB_ADMIN_LOGIN}'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
-# Stop MariaDB service (optional, depending on how you want the container to behave)
-echo "Stopping MariaDB service..."
-service mariadb stop
+# Leave MariaDB running in the foreground
+echo "MariaDB setup complete. Running MariaDB in the foreground."
 
-# Start MariaDB in the foreground using mysqld_safe to keep the container alive
-echo "Starting MariaDB in safe mode..."
-exec mysqld_safe 
+exec mysqld_safe
